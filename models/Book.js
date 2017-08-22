8fd19c233b4ac34e50bedcae6b8ca22bfc1be8db
Stuart Sackler <stuart.sackler@gmail.com>
2017-08-21 22:25:14 -0400

    added the book model.  selected book info to be added

---
+++ models/Book.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+const validator = require('validator');
+const mongoosedbErrorHandler = require('mongoose-mongodb-errors');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    required: 'Please supply a book title'
+  },
+  firstName: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: 'Please supply a first name'
+  },
+  lastName: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: 'Please supply a last name'
+  },
+  genre: {
+    type: String,
+    require: 'Please supply a genre',
+    trim: true
+  }
+});
